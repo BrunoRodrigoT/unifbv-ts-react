d227ab7d632198b84dd5a29b44515bfa1a5b1db1
BrunoRodrigoT <brunorodrigoteixeira2021@gmail.com>
2024-08-31 15:48:54 -0300

    ✨feature: to do context

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,61 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 function App() {
   const validations = z.object({
-    name: z.string().min(3, "Name must be at least 3 characters"),
-    email: z.string().email("Invalid email"),
-    notes: z.string().optional(),
+    name: z.string().min(3, "O nome deve conter ao menos 3 caracteres"),
+    title: z.string().min(3, "O título deve conter ao menos 3 caracteres"),
+    describe: z.string().optional(),
   });
 
   type FormValues = z.infer<typeof validations>;
+  const defaultValues = {
+    name: "",
+    title: "",
+    describe: "",
+  };
 
   const {
     register,
+    reset,
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>({
-    defaultValues: {
-      name: "",
-      email: "",
-      notes: "",
-    },
+    defaultValues,
     resolver: zodResolver(validations),
   });
 
-  const [values, setValues] = React.useState<FormValues>();
+  const [values, setValues] = React.useState<FormValues[]>([]);
+  const [itemUpdate, setItemUpdate] = React.useState<
+    FormValues & { id: number }
+  >();
 
-  const onSubmit = handleSubmit((data) => setValues(data));
+  const deleteItem = (index: number) => {
+    setValues((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const updateItem = (index: number, data: FormValues) => {
+    setValues((prev) => {
+      const newValues = [...prev];
+      newValues[index] = data;
+      return newValues;
+    });
+  };
+
+  const onUpdate = (index: number) => {
+    const data = values[index];
+    setItemUpdate({ ...data, id: index });
+    reset(data);
+  };
+
+  const onSubmit = handleSubmit((data) => {
+    setValues((prev) => [...prev, data]);
+    reset();
+  });
+
+  const onSubmitUpdate = handleSubmit((data) => {
+    if (itemUpdate) updateItem(itemUpdate.id, data);
+    setItemUpdate(undefined);
+    reset(defaultValues);
+  });
 
   return (
     <div>
@@ -51,20 +83,49 @@ function App() {
           helperText={errors.name?.message}
         />
         <Input
-          label="Email"
-          placeholder="Name"
-          {...register("email")}
-          helperText={errors.email?.message}
+          label="Título"
+          placeholder="Escreva o Título"
+          {...register("title")}
+          helperText={errors.title?.message}
         />
         <Input
-          label="Notas"
-          placeholder="Name"
-          {...register("notes")}
-          helperText={errors.notes?.message}
+          label="Descrição"
+          placeholder="Escreva a descrição"
+          {...register("describe")}
+          helperText={errors.describe?.message}
         />
-        <Button onClick={onSubmit}>Entrar</Button>
+        <Button onClick={itemUpdate ? onSubmitUpdate : onSubmit}>
+          {itemUpdate ? "Atualizar" : "Cadastrar"}
+        </Button>
 
-        {values && <p>{JSON.stringify(values)}</p>}
+        {values.map((value, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "40vw",
+              gap: "0.2rem",
+            }}
+          >
+            <p>Número: {index + 1}</p>
+            <p>Nome: {value.name}</p>
+            <p>Título: {value.title}</p>
+            <p>Descrição: {value.describe}</p>
+            <Button
+              onClick={() => deleteItem(index)}
+              style={{ backgroundColor: "red" }}
+            >
+              Excluir
+            </Button>
+            <Button
+              onClick={() => onUpdate(index)}
+              style={{ backgroundColor: "blue" }}
+            >
+              Atualizar
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   );
